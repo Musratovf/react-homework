2d88b656209e6a8ccc8ce4af8eeb9f7d7d2742d6
Musratov <musratof@gmail.com>
2022-03-02 15:30:25 +0500

    new style add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Section from './Component/SectionTop/Section';
+import Position from './Component/PositionMain/Position';
 
 function App() {
   return (
     <>
       <Section />
+      <Position/>
     </>
   );
 }
---
+++ src/Component/PositionMain/Position.jsx
@@ -0,0 +1,44 @@
+function Position() {
+    return (
+        <>
+        <div className="position__container">
+            <div className="position__wrap">
+                <div className="wrap__texts">
+                <h2>Your reliable web and app development partner</h2>
+                <h4>We make successful products that turn great ideas into user-friendly solutions for consumers and businesses.</h4>
+                </div>
+                <hr className="line" />
+                <ul className="wrap__list">
+                    <li>
+                    <p>Completely tailored</p>
+                     <p>software.</p>
+                    </li>
+                    <li>
+                    <p>Testing and quality</p> 
+                    <p>assurance.</p>
+                    </li>
+                </ul>
+                <ul className="wrap__list">
+                    <li>
+                    <p>Projects delivered on time, always.</p>
+                     <p>manager.</p>
+                    </li>
+                    <li>
+                    <p>Professional marketing expertise.</p>
+                    </li>
+                </ul>
+                <ul className="wrap__list">
+                    <li>
+                    <p>Projects delivered on time, </p>
+                     <p>always.</p>
+                    </li>
+                    <li>
+                    <p>Transparent costs and support.</p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        </>
+    )
+}
+export default Position;
